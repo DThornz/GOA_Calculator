84e033d875e85fd8c1c6ed643b35fd399b6b722a
Asad Mirza <asadmirza33165@gmail.com>
2021-02-01 16:30:34 -0500

    Edited image to grayscale and extracting mask

--- main.m
+++ main.m
@@ -10,9 +10,9 @@ height=v.Height; % Height of video in pixels
 allFrames=read(v); % Parse all frames into a 4D uint8 structure
 allFrames_gray=zeros(width,height,fnum);
 for ii=1:fnum
-    allFrames_gray(:,:,ii)=rgb2gray(allFrames(:,:,:,ii));
+    allFrames_gray(:,:,ii)=im2uint8(rgb2gray(allFrames(:,:,:,ii)));
 end
-clear allFrames
+% clear allFrames
 %% Create Calibration Scale
 f=figure(1);
 imshow(allFrames_gray(:,:,1), []);
@@ -40,8 +40,49 @@ fprintf('%f pixels = %f %s \n', ...
 fprintf('Calibration Scale = %f %s/pixels \n',spatialCalibration,units);
 close(f)
 %% Process Single Image
-im=allFrames_gray(:,:,1);
-imshow(im,[])
+im=allFrames(:,:,150); % Load 1st frame
+im2=imadjust(im,[50 250]/255); % Adjust frame contrast 
+im3=imgaussfilt(im2,1); % Add gaussian blur to image
+% im4=imbinarize(im3);
+
+X=im3; % Temporarily image as X
+
+% Graph cut
+foregroundInd = [8217 8219 8222 8223 8226 8228 8231 8234 8236 8237 8239 8243 8245 8741 8744 8745 8746 8776 8778 8780 8782 9268 9269 9794 9795 10321 10322 10847 10848 10850 11374 11375 11376 11902 11903 12431 12960 12961 13488 13489 14018 14547 ];
+backgroundInd = [2698 2699 2701 2702 2704 3223 3224 3239 3752 4280 4808 5336 5864 6393 6394 6922 7981 8510 8511 9039 10098 10628 11157 11686 12216 12745 12746 13275 13805 14865 15395 15925 16455 ];
+L = superpixels(X,2909);
+BW = lazysnapping(X,L,foregroundInd,backgroundInd);
+
+% Flood fill
+row = 65;
+column = 46;
+tolerance = 255;
+weightImage = graydiffweight(X, column, row, 'GrayDifferenceCutoff', tolerance);
+addedRegion = imsegfmm(weightImage, column, row, 0.01);
+BW = BW | addedRegion;
+
+% Invert mask
+BW = imcomplement(BW);
+
+% Active contour
+iterations = 100;
+BW = activecontour(X, BW, iterations, 'Chan-Vese');
+
+% Create masked image.
+maskedImage = X;
+maskedImage(~BW) = 0;
+
+im4=imbinarize(maskedImage);
+pixelArea = bwarea(im4);
+pixelArea = pixelArea*spatialCalibration^2;
+fprintf('GOA = %f %s%c \n',pixelArea*.01,'cm',178);
+
+
+imshow(im);
+green = cat(3, ones(size(im)), zeros(size(im)), zeros(size(im)));
+hold on
+h=imshow(green);
+set(h, 'AlphaData', im4)
 
 %% Batch Process Remaining Images
 %% Plot GOA over Time
