a96b8bf2d9315439555035a5eaca2bbfff95edcb
Asad Mirza <asadmirza33165@gmail.com>
2021-02-01 19:06:35 -0500

    Updated code to use print rather than imwrite

--- save_img_code.m
+++ save_img_code.m
@@ -2,6 +2,4 @@ imshow(im4);
 f=gcf;
 f.Position=[0 0 1080*2 2*1080];
 f.Color='w';
-frame=getframe(f);
-frame=frame.cdata;
-imwrite(frame,'Binarized.tiff');
+print(f,'Binarized.tiff','-dtiff','-r600');  
