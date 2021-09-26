8fe4a0a7c4f2a40d2bfee33e2952054dfa77252e
Asad Mirza <asadmirza33165@gmail.com>
2021-09-26 17:32:17 -0400

    Small edits to reliability of code

--- main.m
+++ main.m
@@ -22,6 +22,8 @@ im=allFrames_gray(:,:,1);
 [spatialCalibration, units]=calibrate_scale(im);
 %% Process Single Image
 im=allFrames_gray(:,:,100); % Load test frame
+% im=imbinarize(im,.98);
+% im=im2uint8(im);
 [im_binarized, pixelArea]=process_img(im,spatialCalibration);
 %% Overlay original grayscale image with mask (in red)
 imshow(im); 
@@ -42,6 +44,8 @@ for ii=1:fnum
     % Update waitbar and message
     waitbar(ii/fnum,f,sprintf('Processing image %d out of %d',ii,fnum))
     im=allFrames_gray(:,:,ii); % Load frame
+%     im=imbinarize(im,.98);
+%     im=im2uint8(im);
     [im_binarized, pixelArea]=process_img(im,spatialCalibration);
     % Save image to output
     im_mask_out(:,:,ii)=im_binarized;    
@@ -52,9 +56,11 @@ delete(f)
 close all force
 %% Plot GOA over Time and Prep for Video
 frames=1:fnum;
-time=0.04*frames/fps;
+% time=frames/fps/3.3;
+time=frames/fps;
 f=figure('units','pixels','position',[0 0 1920*2 2*1080/1.5]);
-% subplot(1,2,1)
+% f=figure;
+subplot(1,2,1)
 g=gca;
 p=plot(time,smooth(pixelArea_out)/100);
 hold on
@@ -79,25 +85,24 @@ set(h, 'AlphaData', im_mask_out(:,:,100))
 
 %% Plot and Create Movie
 % create the video writer with 1 fps
-% writerObj = VideoWriter('GOA_Video.avi','Uncompressed AVI');
-% writerObj.FrameRate = 10;
-% % set the seconds per image
-% % open the video writer
-% open(writerObj);
-% for ii=1:fnum
-%     subplot(1,2,1)
-%     hold on
-%     p2=plot(time(ii),smooth(pixelArea_out(ii)),'g.','MarkerSize',80);
-%     subplot(1,2,2)
-%     imshow(im);
-%     red = cat(3, ones(size(im)), zeros(size(im)), zeros(size(im)));
-%     hold on
-%     h=imshow(red);
-%     set(h, 'AlphaData', im_mask_out(:,:,ii))
-%     drawnow
-%     frame = getframe(gcf) ;
-%     writeVideo(writerObj, frame);
-%     delete(p2);
-% end
-% % close the writer object
-% close(writerObj);
+writerObj = VideoWriter('GOA_Video.avi','Uncompressed AVI');
+writerObj.FrameRate = 10;
+% set the seconds per image
+% open the video writer
+open(writerObj);
+for ii=1:fnum
+    subplot(1,2,1)
+    p2=plot(time(ii),smooth(pixelArea_out(ii)/100),'g.','MarkerSize',80);
+    subplot(1,2,2)
+    imshow(im);
+    red = cat(3, ones(size(im)), zeros(size(im)), zeros(size(im)));
+    hold on
+    h=imshow(red);
+    set(h, 'AlphaData', im_mask_out(:,:,ii))
+    drawnow
+    frame = getframe(gcf) ;
+    writeVideo(writerObj, frame);
+    delete(p2);
+end
+% close the writer object
+close(writerObj);
